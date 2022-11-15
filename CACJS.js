2851da8b8b47d360d10f902d56318c48cc7668f6
Irian Durian <durianirian@gmail.com>
2022-11-15 00:25:40 -0600

    Update CACJS.js

---
+++ CACJS.js
@@ -0,0 +1,4 @@
+int carCost = document.getElementById("carCost").value;
+int yearlyIncome = document.getElementById("yearlyIncome").value;
+int tradeIn = document.getElementById("tradeIn").value;
+int insurance = document.getElementById("insurance").value;
