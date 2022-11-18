667699f36287d54f6004cfff0ef4c130b49c0296
Irian <durianirian@gmail.com>
2022-11-17 18:35:28 -0600

    Update

--- CACJS.js
+++ CACJS.js
@@ -2,3 +2,6 @@ int carCost = document.getElementById("carCost").value;
 int yearlyIncome = document.getElementById("yearlyIncome").value;
 int tradeIn = document.getElementById("tradeIn").value;
 int insurance = document.getElementById("insurance").value;
+
+int twentyPercentofUsersIncome = yearlyIncome * 0.2;
+int carCostMinusTradeIn = carCost - tradeIn;
