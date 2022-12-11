16a22d1cbaa5f0a38f404a28f13d77b9ced98cdc
Irian Durian <durianirian@gmail.com>
2022-12-10 19:03:16 -0600

    Don't know how to display 20% of users income

--- CACJS.js
+++ CACJS.js
@@ -1,7 +1,21 @@
-int carCost = document.getElementById("carCost").value;
-int yearlyIncome = document.getElementById("yearlyIncome").value;
-int tradeIn = document.getElementById("tradeIn").value;
-int insurance = document.getElementById("insurance").value;
+document.getElementById("calculate").onclick = calculate;
 
-int twentyPercentofUsersIncome = yearlyIncome * 0.2;
-int carCostMinusTradeIn = carCost - tradeIn;
+function calculate() {
+  var carCost = document.getElementById("carCost").value;
+  var yearlyIncome = document.getElementById("yearlyIncome").value;
+  var tradeIn = document.getElementById("tradeIn").value;
+  var insurance = document.getElementById("insurance").value;
+  // convert all the values to numbers
+  carCost = Number(carCost);
+  yearlyIncome = Number(yearlyIncome);
+  tradeIn = Number(tradeIn);
+  insurance = Number(insurance);
+
+  var TwentyPerOfIncome = yearlyIncome * 0.2;
+
+  // Get the element where we want to display the result
+  var TwentyPerOfIncome_Output = document.getElementById("twentyPercent");
+
+  // Displays the result
+  TwentyPerOfIncome_Output.innerHTML = TwentyPerOfIncome;
+}
