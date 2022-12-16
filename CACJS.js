5d70c99e1ea8b8a0f8bb69876b08ec47ef81d47d
Irian Durian <durianirian@gmail.com>
2022-12-15 20:12:02 -0600

    Fixed the button and output for yearlyIncome

--- CACJS.js
+++ CACJS.js
@@ -1,21 +1,32 @@
 document.getElementById("calculate").onclick = calculate;
 
 function calculate() {
+  // Get the values from the processInputValues() function
+  var { carCost, yearlyIncome, tradeIn, insurance } = processInputValues();
+  // Calculate 20% on yearly income
+  var TwentyPerOfIncome = yearlyIncome * 0.2;
+  // Get the element where we want to display the result
+  var TwentyPerOfIncome_Output = document.getElementById("twentyPercent");
+  // Displays the result
+  TwentyPerOfIncome_Output.innerText = TwentyPerOfIncome;
+}
+
+function processInputValues() {
+  // Get the values from the input fields
   var carCost = document.getElementById("carCost").value;
   var yearlyIncome = document.getElementById("yearlyIncome").value;
   var tradeIn = document.getElementById("tradeIn").value;
   var insurance = document.getElementById("insurance").value;
-  // convert all the values to numbers
-  carCost = Number(carCost);
-  yearlyIncome = Number(yearlyIncome);
-  tradeIn = Number(tradeIn);
-  insurance = Number(insurance);
+  // Remove the non-digit characters from each value
+  carCost = carCost.replace(/[^0-9]/g, "");
+  yearlyIncome = yearlyIncome.replace(/[^0-9]/g, "");
+  tradeIn = tradeIn.replace(/[^0-9]/g, "");
+  insurance = insurance.replace(/[^0-9]/g, "");
+  // convert all the values to ints
+  carCost = parseInt(carCost);
+  yearlyIncome = parseInt(yearlyIncome);
+  tradeIn = parseInt(tradeIn);
+  insurance = parseInt(insurance);
 
-  var TwentyPerOfIncome = yearlyIncome * 0.2;
-
-  // Get the element where we want to display the result
-  var TwentyPerOfIncome_Output = document.getElementById("twentyPercent");
-
-  // Displays the result
-  TwentyPerOfIncome_Output.innerHTML = TwentyPerOfIncome;
+  return { carCost, yearlyIncome, tradeIn, insurance };
 }
