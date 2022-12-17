17ef9ad6b7f1df27b0e39424549ce7862d910ebf
Irian Durian <durianirian@gmail.com>
2022-12-16 18:54:24 -0600

    Changing up my questions and calculations

--- CACJS.js
+++ CACJS.js
@@ -1,33 +1,37 @@
 document.getElementById("calculate").onclick = calculate;
 
-function calculate() {
-  // Get the values from the processInputValues() function
-  var { carCost, yearlyIncome, tradeIn, insurance } = processInputValues();
-  // Calculate the total cost of the car
-  var carCostWithInsurance = carCost + insurance;
-  // Calculate 20% on yearly income
-  var TwentyPerOfIncome = yearlyIncome * 0.2;
-
-  document.getElementById("carCostWithInsurance").innerText = carCostWithInsurance;
-  document.getElementById("twentyPercent").innerText = TwentyPerOfIncome;
-}
-
 function processInputValues() {
   // Get the values from the input fields
-  var carCost = document.getElementById("carCost").value;
+  var monthlyPayment = document.getElementById("monthlyPayment").value;
   var yearlyIncome = document.getElementById("yearlyIncome").value;
   var tradeIn = document.getElementById("tradeIn").value;
   var insurance = document.getElementById("insurance").value;
+  
   // Remove the non-digit characters from each value
-  carCost = carCost.replace(/[^0-9]/g, "");
+  monthlyPayment = monthlyPayment.replace(/[^0-9]/g, "");
   yearlyIncome = yearlyIncome.replace(/[^0-9]/g, "");
   tradeIn = tradeIn.replace(/[^0-9]/g, "");
   insurance = insurance.replace(/[^0-9]/g, "");
+  
   // convert all the values to integers
-  carCost = parseInt(carCost);
+  monthlyPayment = parseInt(monthlyPayment);
   yearlyIncome = parseInt(yearlyIncome);
   tradeIn = parseInt(tradeIn);
   insurance = parseInt(insurance);
 
-  return { carCost, yearlyIncome, tradeIn, insurance };
+  return { monthlyPayment, yearlyIncome, tradeIn, insurance };
+}
+
+function displayResults(TwentyPerOfIncome) {
+  document.getElementById("twentyPercent").innerText = TwentyPerOfIncome;
+}
+
+function calculate() {
+  // Get the values from the processInputValues() function
+  var { monthlyPayment, yearlyIncome, tradeIn, insurance } = processInputValues();
+  // Calculate 20% on yearly income
+  var TwentyPerOfIncome = yearlyIncome * 0.2;
+
+  // Call the displayResults function, passing in the values to display
+  displayResults(TwentyPerOfIncome);
 }
