425bc00b732dfbdfd4dc82483712ef35312b615a
Irian Durian <durianirian@gmail.com>
2022-12-17 19:34:49 -0600

    Added new inputs

--- CACJS.js
+++ CACJS.js
@@ -1,34 +1,47 @@
 document.getElementById("calculate").onclick = calculate;
 
+// make variables for credit score ranges
+var excellent = creditScore >= 800
+var veryGood = creditScore >= 740 && creditScore < 800;
+var good = creditScore >= 670 && creditScore < 740;
+var fair = creditScore >= 580 && creditScore < 670;
+var poor = creditScore >= 0 && creditScore < 580;
+
 function processInputValues() {
   // Get the values from the input fields
-  var monthlyPayment = document.getElementById("monthlyPayment").value;
+  var carCost = document.getElementById("carCost").value;
   var yearlyIncome = document.getElementById("yearlyIncome").value;
   var tradeIn = document.getElementById("tradeIn").value;
-  var insurance = document.getElementById("insurance").value;
+  var downPayment = document.getElementById("downPayment").value;
+  var creditScore = document.getElementById("creditScore").value;
+  var loan = document.getElementById("loan").value;
   
   // Remove the non-digit characters from each value
-  monthlyPayment = monthlyPayment.replace(/[^0-9]/g, "");
+  carCost = carCost.replace(/[^0-9]/g, "");
   yearlyIncome = yearlyIncome.replace(/[^0-9]/g, "");
   tradeIn = tradeIn.replace(/[^0-9]/g, "");
-  insurance = insurance.replace(/[^0-9]/g, "");
+  downPayment = downPayment.replace(/[^0-9]/g, "");
+  creditScore = creditScore.replace(/[^0-9]/g, "");
+  loan = loan.replace(/[^0-9]/g, "");
   
   // convert all the values to integers
-  monthlyPayment = parseInt(monthlyPayment);
+  carCost = parseInt(carCost);
   yearlyIncome = parseInt(yearlyIncome);
   tradeIn = parseInt(tradeIn);
-  insurance = parseInt(insurance);
-
-  return { monthlyPayment, yearlyIncome, tradeIn, insurance };
+  downPayment = parseInt(downPayment);
+  creditScore = parseInt(creditScore);
+  loan = parseInt(loan);
+  
+  return { carCost, yearlyIncome, tradeIn, downPayment };
 }
 
 function displayResults(TwentyPerOfIncome) {
-  document.getElementById("twentyPercent").innerText = TwentyPerOfIncome;
+document.getElementById("twentyPercent").innerText = TwentyPerOfIncome;
 }
 
 function calculate() {
   // Get the values from the processInputValues() function
-  var { monthlyPayment, yearlyIncome, tradeIn, insurance } = processInputValues();
+  var { carCost, yearlyIncome, tradeIn, downPayment } = processInputValues();
   // Calculate 20% on yearly income
   var TwentyPerOfIncome = yearlyIncome * 0.2;
 
