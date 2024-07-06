6539812b30b5b695447ef6a12283cdd9ef173c5e
Irian Durian <durianirian@gmail.com>
2024-07-06 11:11:47 -0500

    Remove unused components and update page layout

--- src/app/page.server.tsx
+++ src/app/page.server.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import Background from "../../components/background.server";
-import Calculator from "../../components/calculator.client";
+import Calculator from "../components/calculator.client";
 
 export default function Page() {
   return (
     <html lang="en">
       <body>
         <h1>Car affordability calculator</h1>
-        <Background />
         <Calculator />
       </body>
     </html>
